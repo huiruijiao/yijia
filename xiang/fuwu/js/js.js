c5c293bd7f46f08f4360140cdbb8448c86cbaa53
huirujiao <154954486@qq.com>
2017-07-06 17:09:21 +0800

    7-6

---
+++ xiang/fuwu/js/js.js
@@ -0,0 +1,94 @@
+$(function(){
+	$(document).ready(function(){
+  		$('.top-t-l').mousemove(function(){
+  		$('#tu').slideDown(200);//可以设置切换时间 
+  	});
+  		$('.top-t-l').mouseleave(function(){
+  		$('#tu').slideUp("fast");
+  	});  	 	
+ 	
+});
+// ActionScript Document
+$(".ding").on("mouseenter", function() {
+                $('#xia1').show();
+		
+            })
+$(".ding").on("mouseleave", function() {
+                $('#xia1').hide();
+            });
+$("#xia1").on("mouseenter", function() {
+                $(this).show();
+            })
+$("#xia1").on("mouseleave", function() {
+                $(this).hide();
+            });
+	
+
+
+
+$(".ding1").on("mouseenter", function() {
+                $('#xia2').show();
+		
+            })
+$(".ding1").on("mouseleave", function() {
+                $('#xia2').hide();
+            });
+$("#xia2").on("mouseenter", function() {
+                $(this).show();
+            })
+$("#xia2").on("mouseleave", function() {
+                $(this).hide();
+            });
+	})
+
+
+var _box_y = $("#subnav").offset().top; 
+var _box_x = $("#subnav").offset().left; 
+var a = $("i").offset().top;
+var a = $("i").offset().left;
+$(window).scroll(function(){ 
+if($(window).scrollTop() > _box_y){ 
+$("#subnav").attr("style","position: absolute;top:"+($(window).scrollTop()-6)+"px; left:"+_box_x+"px;z-index:99;width:100%;height:1.5%;"); 
+$("i").hide("slow");
+}else{ 
+$("#subnav").attr("style","");
+$("i").show("slow");
+} 
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ xiang/zhuce/js/zhuce.js
@@ -0,0 +1,74 @@
+
+	$('input').blur(function(){
+             var $parent = $(this).parent();
+             $parent.find(".formtips").remove();
+             //验证用户名
+             
+             if( $(this).is('#username') ){
+             		var reg = /^1[35784][0-9]{9}$/;
+             		//var you = /^\w+@\w+\.(com|net|cn|com\.cn)$/;
+						//console.log(typeof(you))
+					
+                    if( reg.test(this.value)==false){                   	
+                        var errorMsg = '请输入电话号码或邮箱';
+                        $(".span").html(errorMsg);
+                        $(".span").css("color","red");
+                        $("#username").css("background","#fed9d9")
+                        $(".span").css("display","block");
+                    }else{
+                        var okMsg = '√';
+                        $(".span").html(okMsg);
+                        $(".span").css("color","green");
+                        $(".span").css("display","block");
+                    }
+                    
+             }
+             //验证密码
+           	if($(this).is('.mm')){
+           		console.log(this.placeholder)
+           		
+           		if(this.value=="" || this.value.length<6){
+           			var errorMsg = '密码最少6位';
+                        $(".span1").html(errorMsg);
+                        $(".span1").css("color","red");
+                         $(".span1").css("display","block");
+                     	$("#mm").css("background","#fed9d9")
+                        
+           		}else{
+           			var errorMsg = '√';
+                        $(".span1").html(errorMsg);
+                        $(".span1").css("color","green");
+                         $(".span1").css("display","block");
+           		}
+           		
+           }
+           if( $(this).is('.qr') ){
+           		//console.log($("#put2")[0].value)
+           		//console.log(this.value)
+             	if(this.value==($(".mm")[0].value) && this.value!=""){
+             		var errorMsg = '√';
+                        $(".span2").html(errorMsg);
+                        $(".span2").css("color","green");
+                         $(".span2").css("display","block");
+             	}else{
+             		var errorMsg = '输入错误';
+                        $(".span2").html(errorMsg);
+                        $(".span2").css("color","red");
+                        $("#qr").css("background","#fed9d9")
+                        $(".span2").css("display","block");
+             	}
+             }
+         
+        })
+		$("#username").focus(function(){
+  			$("#username").css("background-color","#fff");
+  			$(".span").css("display","none");
+		});
+		$("#mm").focus(function(){
+  			$("#mm").css("background-color","#fff");
+  			$(".span1").css("display","none");
+		});
+		$("#qr").focus(function(){
+  			$("#qr").css("background-color","#fff");
+  			$(".span2").css("display","none");
+		});
